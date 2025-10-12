b95ca3b14ca6ff9d4a24f8d157c45328562a7b14
Gyon Shayel Joshua <gyon.perera2002@gmail.com>
2025-10-12 12:51:24 +0400

    Implement search functionality

--- src/main.js
+++ src/main.js
@@ -4,36 +4,48 @@ import "../styles/components/header.css";
 import "../styles/components/weather-data.css";
 import "../styles/utils.css";
 
-async function getWeatherInfo() {
-  const url = "http://api.weatherstack.com/current";
+const searchBtn = document.getElementById("search-btn");
+const search = document.getElementById("search");
+
+searchBtn.addEventListener("click", () => {
+  const city = search.value.trim();
+
+  if (!city) {
+    search.placeholder = "Please enter a city name";
+  } else {
+    getWeatherInfo(city);
+    search.value = "";
+    search.placeholder = "Search for a city";
+  }
+});
+
+async function getWeatherInfo(query) {
+  const url = "https://api.weatherstack.com/current";
   const apiKey = "9d8315a37b402b7dcf67cba7aa57b80f";
   const weatherDataContainer = document.querySelector(".weather-data");
-  let city = "New York";
-  let html = "";
-  let data = "";
+  let weatherDataHTML = "";
+  let weatherData = "";
 
-  const apiCall = `${url}?access_key=${apiKey}&query=${city}`;
+  const apiCall = `${url}?access_key=${apiKey}&query=${query}`;
 
   try {
     const response = await fetch(apiCall);
-    if (response.ok) {
-      data = await response.json();
-      console.log(data);
+    if (!response.ok) {
+      throw new Error("City not found.");
+    } else {
+      weatherData = await response.json();
+      console.log(weatherData); // Remove later
 
-      html = `
-        <h1 class="weather-data__city">${data.location.name}</h1>
-        <h2 class="weather-data__temp">&nbsp;&nbsp;${data.current.temperature}°</h2>
-        <p class="weather-data__summary">${data.current.weather_descriptions[0]}</p>
-        <p class="weather-data__highlowtemp">H:${data.current.temperature}° L:${data.current.temperature}°</p>
+      weatherDataHTML = `
+        <h1 class="weather-data__city">${weatherData.location.name}</h1>
+        <h2 class="weather-data__temp">&nbsp;${weatherData.current.temperature}°</h2>
+        <p class="weather-data__summary">${weatherData.current.weather_descriptions[0]}</p>
+        <p class="weather-data__highest-lowest-temp">H:${weatherData.current.temperature}° L:${weatherData.current.temperature}°</p>
       `;
 
-      weatherDataContainer.innerHTML = html;
-    } else {
-      alert(`Error: ${data.error.info}`);
+      weatherDataContainer.innerHTML = weatherDataHTML;
     }
   } catch (error) {
-    console.error("Error fetching weather data:", error);
+    alert(`Error fetching weather data: ${error.message}`);
   }
 }
-
-getWeatherInfo();
